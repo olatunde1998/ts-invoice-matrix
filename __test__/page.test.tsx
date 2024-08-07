f038e99b7c28af36283c14374d17b2025fb1d3b0
geodevcode <geodevinstitute@gmail.com>
2024-08-07 09:38:27 +0100

    testing library (Jest) setup configuration done

---
+++ __test__/page.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Home from "@/app/page";
+
+describe("Dashboard Page Component", () => {
+  test("Should renders Properly", () => {
+    const { getByText } = render(<Home />);
+    // Test for the welcome message
+    const welcomeMessage = getByText("Welcome to Total Secure Invoice Matrix");
+    expect(welcomeMessage).toBeInTheDocument();
+  });
+});
