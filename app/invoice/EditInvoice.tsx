a2e5c2839c183895b56b1bd7eab1d8e3c92074e8
geodevcode <geodevinstitute@gmail.com>
2024-08-08 08:17:25 +0100

    EditInvoice page created

---
+++ app/invoice/EditInvoice.tsx
@@ -0,0 +1,13 @@
+interface EditInvoiceProps {
+  setShowEditInvoice?: any;
+}
+
+export default function EditInvoice({ setShowEditInvoice }: EditInvoiceProps) {
+  return (
+    <>
+      <div>
+        <p className="text-red-800">This is Edit Invoice Sheet</p>
+      </div>
+    </>
+  );
+}
