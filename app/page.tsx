339a8076efc2cc853d3fa3c4c34c586c565b0635
geodevcode <geodevinstitute@gmail.com>
2024-08-08 12:03:36 +0100

    HeaderCrumb added and trash component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
-import Link from "next/link";
+import HeaderCrumb from "@/app/components/header-crumb/HeaderCrumb";
+import Trash from "@/app/components/trash/Trash";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to Total Secure Invoice Matrix</p>
-    </div>
+    <main className="min-h-screen px-4 p-2 md:p-8 z-999">
+      <div>
+        <HeaderCrumb
+          screenName="Dashboard"
+          screenContent="Software and tools I use on a regular basis."
+          buttonName="View Now"
+        />
+      </div>
+      <section className="h-fit mt-8 p-6">
+        <div className="lg:mt-20">
+          {/* ====EMPTY TRASH GOES HERE === */}
+          <Trash
+            headingText="Start Adding Users"
+            subHeadingText={
+              <span>
+                No clients have been added yet. Click the{" "}
+                <span className="text-primary mx-1">'View More'</span> button
+                above to view more.
+              </span>
+            }
+          />
+        </div>
+      </section>
+    </main>
   );
 }
