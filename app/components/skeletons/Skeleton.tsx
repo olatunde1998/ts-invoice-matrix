e3868c8e10cb8a79f53e99628ec238d692398151
geodevcode <geodevinstitute@gmail.com>
2024-08-08 12:15:04 +0100

    dark mode background color added

--- app/components/skeletons/Skeleton.tsx
+++ app/components/skeletons/Skeleton.tsx
@@ -1,18 +1,18 @@
 export const Skeleton = () => {
   return (
     <div className="relative -z-[9999] w-full rounded overflow-hidden shadow-lg">
-      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
-      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
-      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
-      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
-      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
-      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
-      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
-      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
-      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
-      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
-      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
-      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 dark:bg-accent animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 dark:bg-accent animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 dark:bg-accent animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 dark:bg-accent animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 dark:bg-accent animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 dark:bg-accent animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 dark:bg-accent animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 dark:bg-accent animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 dark:bg-accent animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 dark:bg-accent animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 dark:bg-accent animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 dark:bg-accent animate-pulse"></div>
     </div>
   );
 };
