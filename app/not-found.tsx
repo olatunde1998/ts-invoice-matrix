22aba68a960baa68c5585cdcd2d8fefff8ba64b4
geodevcode <geodevinstitute@gmail.com>
2024-08-08 07:14:24 +0100

    404 Page created

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="bg-[#0F172A] h-screen pt-16">
+    <div className="bg-[#0F172A] h-screen pt-16 fixed inset-0 z-[1005]">
       <div className="flex items-center justify-center  flex-col max-w-[700px] mx-auto px-5 my-8">
         <div className="max-w-md md:max-w-lg text-center">
           <h2 className="text-9xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-pink-500 text-[128px] font-bold mb-8 ">
