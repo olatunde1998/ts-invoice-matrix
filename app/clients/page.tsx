8eadeec206f32ab95037ea69cb70922f3fce2181
geodevcode <geodevinstitute@gmail.com>
2024-08-08 11:56:32 +0100

    Clients Page created

---
+++ app/clients/page.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import HeaderCrumb from "@/app/components/header-crumb/HeaderCrumb";
+import Trash from "@/app/components/trash/Trash";
+
+export const metadata: Metadata = {
+  title: "Clients | geodevcodes",
+  description: "Total Secure Application",
+};
+
+export default function Clients() {
+  return (
+    <main className="min-h-screen px-4 p-2 md:p-8">
+      <div>
+        <HeaderCrumb
+          screenName="Clients"
+          screenContent="Administer client account within the platform."
+          buttonName="Add Client"
+        />
+      </div>
+      <section className="h-fit mt-8 p-6">
+        <div className="lg:mt-20">
+          {/* ====EMPTY TRASH GOES HERE === */}
+          <Trash
+            headingText="Start Adding Clients"
+            subHeadingText={
+              <span>
+                No clients have been added yet. Click the{" "}
+                <span className="text-primary mx-1">'Add Client'</span> button
+                above to create a new client.
+              </span>
+            }
+          />
+        </div>
+      </section>
+    </main>
+  );
+}
