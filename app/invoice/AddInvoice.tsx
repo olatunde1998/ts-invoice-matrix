b9bc815fe9cc5ade6bf32508e0e002d8cd05c749
geodevcode <geodevinstitute@gmail.com>
2024-08-08 08:17:08 +0100

    AddInvoice Page created

---
+++ app/invoice/AddInvoice.tsx
@@ -0,0 +1,13 @@
+interface AddInvoiceProps {
+  setShowAddInvoice?: any;
+}
+
+export default function AddInvoice({ setShowAddInvoice }: AddInvoiceProps) {
+  return (
+    <>
+      <div>
+        <p className="text-red-800">This is Add Invoice Sheet</p>
+      </div>
+    </>
+  );
+}
