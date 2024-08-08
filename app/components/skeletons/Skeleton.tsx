a69dcaa9ee3881f364f2e6c037b24d3d51da1a27
geodevcode <geodevinstitute@gmail.com>
2024-08-08 08:40:49 +0100

    Skeleton component created

---
+++ app/components/skeletons/Skeleton.tsx
@@ -0,0 +1,18 @@
+export const Skeleton = () => {
+  return (
+    <div className="relative -z-[9999] w-full rounded overflow-hidden shadow-lg">
+      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
+      <div className="w-full h-10 mb-2 bg-gray-100 animate-pulse"></div>
+    </div>
+  );
+};
