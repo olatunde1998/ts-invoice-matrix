ba55a8325bfc25d6285b10f350af409243ec1d67
geodevcode <geodevinstitute@gmail.com>
2024-08-09 18:31:11 +0100

    Date format conversion logic added

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,14 +1,21 @@
 import { format } from 'date-fns';
 
 
-
+  // FUNCTION TO GET RANDOM EMAIL
 export function getRandomEmail(firstName: string, lastName: string) {
     const domains = ['example.com', 'mail.com', 'test.com'];
     const domain = domains[Math.floor(Math.random() * domains.length)];
     return `${firstName.toLowerCase()}.${lastName.toLowerCase()}@${domain}`;
   }
   
-export function formatDate(dateString: any) {
-    const date = new Date(dateString);
-    return format(date, 'dd MMM, yyyy');
-  }
+
+  // FUNCTION TO FORMAT DATE
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "2-digit",
+  };
+  return date.toLocaleDateString("en-US", options);
+};
