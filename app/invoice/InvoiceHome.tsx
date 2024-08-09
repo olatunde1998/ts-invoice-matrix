570912841502dfde37ef2e441679614050c858eb
geodevcode <geodevinstitute@gmail.com>
2024-08-09 18:32:05 +0100

    fetch all invoices integration implementation done

--- app/invoice/InvoiceHome.tsx
+++ app/invoice/InvoiceHome.tsx
@@ -1,28 +1,32 @@
 "use client";
-import { createColumnHelper } from "@tanstack/react-table";
-import { AiOutlineEdit } from "react-icons/ai";
 import { Trash } from "lucide-react";
-import { Table } from "@/app/components/tables/Table";
-import { invoicesData } from "@/utils/invoiceData";
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
-import { Sheet } from "@/app/components/sheets/Sheet";
+import { GetInvoicesRequest } from "@/app/services/invoice.request";
 import HeaderCrumb from "@/app/components/header-crumb/HeaderCrumb";
+import { Skeleton } from "@/app/components/skeletons/Skeleton";
+import TrashPage from "@/app/components/trash/TrashPage";
+import { createColumnHelper } from "@tanstack/react-table";
+import { Table } from "@/app/components/tables/Table";
+import { Sheet } from "@/app/components/sheets/Sheet";
 import { Modal } from "@/app/components/modals/Modal";
+import { AiOutlineEdit } from "react-icons/ai";
+import { useState } from "react";
 import AddInvoice from "./AddInvoice";
 import EditInvoice from "./EditInvoice";
 import DeleteInvoice from "./DeleteInvoice";
-import { Skeleton } from "@/app/components/skeletons/Skeleton";
+import { formatDate } from "@/utils/utils";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 interface InvoiceData {
-  id: string;
+  _id: string;
   fullName?: any;
   firstName?: string;
   lastName?: string;
   email?: string;
-  date?: string;
+  createdAt?: any;
   amount?: number;
-  paymentStatus?: string;
+  status?: string;
   repositoryUrl?: string;
   files?: string[];
   action?: any;
@@ -33,18 +37,19 @@ export default function InvoiceHome() {
   const [showEditInvoice, setShowEditInvoice] = useState(false);
   const [showDeleteInvoice, setShowDeleteInvoice] = useState(false);
   const [selectedRow, setSelectedRow] = useState<InvoiceData | null>(null);
+
+  const { data: invoiceData, isLoading } = useQuery({
+    queryKey: ["getInvoicesApi"],
+    queryFn: () => GetInvoicesRequest(),
+  });
+
   // create columnHelper
   const columnHelper = createColumnHelper<InvoiceData>();
 
-  const { data: invoiceData, isLoading } = useQuery<InvoiceData[]>({
-    queryKey: ["getInvoiceApi"],
-    queryFn: () => Promise.resolve(invoicesData),
-  });
-
   // Table columns
   const columns = [
-    columnHelper.accessor("id", {
-      cell: (info) => <span>{info?.row?.original?.id}</span>,
+    columnHelper.accessor("_id", {
+      cell: (info) => <span>{info.row.index + 1}</span>,
       header: () => <span>S/N </span>,
     }),
     columnHelper.accessor("fullName", {
@@ -57,8 +62,10 @@ export default function InvoiceHome() {
       cell: (info) => <span>{info?.row?.original?.email}</span>,
       header: () => <span>Email</span>,
     }),
-    columnHelper.accessor("date", {
-      cell: (info) => <span>{info?.row?.original?.date}</span>,
+    columnHelper.accessor("createdAt", {
+      cell: (info) => (
+        <span> {formatDate(info?.row?.original?.createdAt)}</span>
+      ),
       header: () => <span>Account Created Date</span>,
     }),
     columnHelper.accessor("amount", {
@@ -66,15 +73,15 @@ export default function InvoiceHome() {
       header: () => <span>Amount</span>,
     }),
 
-    columnHelper.accessor("paymentStatus", {
+    columnHelper.accessor("status", {
       header: () => <span>Status</span>,
       cell: (info) => (
         <div
           className={`
           ${
-            info?.row?.original?.paymentStatus === "Paid"
+            info?.row?.original?.status === "Paid"
               ? "bg-[#D1FADF] text-[#079455] w-[100px]"
-              : info?.row?.original?.paymentStatus === "Pending"
+              : info?.row?.original?.status === "Pending"
               ? "bg-[#F9EDC4] text-[#D0AA25] w-[114px]"
               : "bg-[#EDEDF1] text-[#6C748B] w-[114px]"
           } text-center p-1 rounded-2xl flex items-center space-x-3 justify-center
@@ -82,14 +89,14 @@ export default function InvoiceHome() {
         >
           <p
             className={` ${
-              info?.row?.original?.paymentStatus === "Paid"
+              info?.row?.original?.status === "Paid"
                 ? "bg-[#079455]"
-                : info?.row?.original?.paymentStatus === "Pending"
+                : info?.row?.original?.status === "Pending"
                 ? "bg-[#D0AA25]"
                 : "bg-[#6C748B]"
             } w-2 h-2 rounded-full`}
           ></p>
-          <p className="capitalize">{info?.row?.original?.paymentStatus}</p>
+          <p className="capitalize">{info?.row?.original?.status}</p>
         </div>
       ),
     }),
@@ -137,25 +144,34 @@ export default function InvoiceHome() {
         />
       </div>
       <section className="h-fit mt-8 ">
-        {/* ====EMPTY TRASH GOES HERE === */}
-        {/* <div className="lg:mt-20">
-          <Trash
-            headingText="Start Adding Invoices"
-            subHeadingText={
-              <span>
-                No invoices have been added yet. Click the{" "}
-                <span className="text-primary mx-1">'Create Invoice'</span>{" "}
-                button above to create a new invoice.
-              </span>
-            }
-          />
-        </div> */}
         {isLoading ? (
-          <div>
-            <Skeleton />
-          </div>
+          <>
+            {/* ====SKELETON GOES HERE === */}
+            <div>
+              <Skeleton />
+            </div>
+          </>
+        ) : invoiceData?.length === 0 ? (
+          <>
+            {/* ====EMPTY TRASH GOES HERE === */}
+            <div className="lg:mt-20">
+              <TrashPage
+                headingText="Start Adding Invoices"
+                subHeadingText={
+                  <span>
+                    No invoices have been added yet. Click the{" "}
+                    <span className="text-primary mx-1">
+                      &apos;Create Invoice&apos;
+                    </span>{" "}
+                    button above to create a new invoice.
+                  </span>
+                }
+              />
+            </div>
+          </>
         ) : (
           <div>
+            {/* ====INVOICE TABLE GOES HERE === */}
             <Table
               data={invoiceData ? invoiceData : []}
               columns={columns}
@@ -170,7 +186,10 @@ export default function InvoiceHome() {
         <AddInvoice setShowAddInvoice={setShowAddInvoice} />
       </Sheet>
       <Sheet show={showEditInvoice} onClose={() => setShowEditInvoice(false)}>
-        <EditInvoice setShowEditInvoice={setShowEditInvoice} />
+        <EditInvoice
+          setShowEditInvoice={setShowEditInvoice}
+          editInvoiceID={selectedRow ? selectedRow._id : null}
+        />
       </Sheet>
 
       {/* ===Delete Modal */}
@@ -180,9 +199,10 @@ export default function InvoiceHome() {
       >
         <DeleteInvoice
           setShowDeleteInvoice={setShowDeleteInvoice}
-          // deleteInvoiceID={selectedRow ? selectedRow.id : null}
+          deleteInvoiceID={selectedRow ? selectedRow._id : null}
         />
       </Modal>
+      <ToastContainer />
     </>
   );
 }
