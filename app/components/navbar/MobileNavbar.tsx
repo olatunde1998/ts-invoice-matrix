4910af2385bdbd72c054f45845617dcb9398babd
geodevcode <geodevinstitute@gmail.com>
2024-08-11 13:43:01 +0100

    company logo image added to navbar

--- app/components/navbar/MobileNavbar.tsx
+++ app/components/navbar/MobileNavbar.tsx
@@ -3,17 +3,26 @@ import Link from "next/link";
 import { ModeToggle } from "../modeToggle/ModeToggle";
 import { MobileSidebar } from "../sidebar/MobileSidebar";
 
-
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 
 export default function MobileNavbar() {
   const pathname = usePathname();
 
   return (
-    <nav className="z-[1000] bg-background fixed px-[20px] py-[10px] top-0 left-0 right-0 border-b border-accent md:hidden">
+    <nav className="z-[1000] bg-background fixed px-[20px] py-[20px] top-0 left-0 right-0 border-b border-accent md:hidden">
       <div className="flex justify-between items-center">
         <Link href="/" className="font-bold ">
-          <span className="text-primary text-md">Rasheed Olatunde</span>
+          <div className="flex items-center">
+            <Image
+              src="/images/ts-brand-logo.png"
+              alt="profile image"
+              width={100}
+              height={100}
+              priority
+              className="w-[100px] h-[50px] object-cover"
+            />
+          </div>
         </Link>
         <div className="flex justify-between gap-x-2">
           <ModeToggle />
