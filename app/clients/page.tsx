338505aab0cca69b78f5edbeafefa0a12219dca1
geodevcode <geodevinstitute@gmail.com>
2024-08-11 09:31:42 +0100

    ClientHome imported and neccessary styling added

--- app/clients/page.tsx
+++ app/clients/page.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
-import HeaderCrumb from "@/app/components/header-crumb/HeaderCrumb";
-import TrashPage from "@/app/components/trash/TrashPage";
+import ClientsHome from "./ClientsHome";
 
 export const metadata: Metadata = {
   title: "Clients | geodevcodes",
@@ -10,30 +9,8 @@ export const metadata: Metadata = {
 export default function Clients() {
   return (
     <main className="min-h-screen px-4 p-2 md:p-8">
-      <div>
-        <HeaderCrumb
-          screenName="Clients"
-          screenContent="Administer client account within the platform."
-          buttonName="Add Client"
-        />
-      </div>
-      <section className="h-fit mt-8 p-6">
-        <div className="lg:mt-20">
-          {/* ====EMPTY TRASH GOES HERE === */}
-          <TrashPage
-            headingText="Start Adding Clients"
-            subHeadingText={
-              <span>
-                No clients have been added yet. Click the{" "}
-                <span className="text-primary mx-1">
-                  &apos;Add Client&apos;
-                </span>{" "}
-                button above to create a new client.
-              </span>
-            }
-          />
-        </div>
-      </section>
+      {/* ==== CLIENT  GOES HERE ===== */}
+      <ClientsHome />
     </main>
   );
 }
